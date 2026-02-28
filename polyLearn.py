ce06114c4e75a1d406faf6a1f0b5b02218873502
JupiterJ <jadenjpt@stanford.edu>
2026-02-28 10:05:11 -0800

    Regressions with splits

--- polyLearn.py
+++ polyLearn.py
@@ -3,6 +3,8 @@ import pandas as pd
 import numpy as np
 import json
 from pandas import json_normalize
+from sklearn import linear_model
+from sklearn.model_selection import train_test_split,KFold,cross_val_score
 
 
 def get_data(filename):
@@ -33,7 +35,8 @@ def get_data(filename):
 
 
 # Main body
-data_filename = 'poly_info_Radiation resistance.json'
+data_filename = 'poly_info_Glass transition temperature.json'
+# data_filename = 'poly_info_Radiation resistance.json'
 df = get_data(data_filename)
 
 # Determines the number of datapoints for each property
@@ -52,7 +55,80 @@ polymer_names = df['polymer_data.polymer_name'].to_numpy()
 polymer_smiles = df['polymer_data.smiles'].to_numpy()
 
 # Check dimensions of all outputs
-print(array.shape)
-print(properties.shape)
-print(polymer_names.shape)
-print(polymer_smiles.shape)
+# print(array.shape)
+# print(properties.shape)
+# print(polymer_names.shape)
+# print(polymer_smiles.shape)
+
+# Selects the polymers used for prediction
+# property_list = ['Melting temperature','Density','Heat of fusion']
+property_list = ['Melting temperature','Density']
+prediction_list = ['Glass transition temperature']
+
+# Selects the properties used for prediction
+# n = [0,1,2]
+n = [0,1]
+
+x = array[:,np.isin(properties,property_list)]
+y = array[:,np.isin(properties,prediction_list)]
+
+# Restricts polymer property array to polymers that have data on all the desired properties
+mask = np.logical_and(~np.isnan(x).any(axis=1),~np.isnan(y).any(axis=1))
+x = x[mask]
+y = y[mask]
+
+print(np.average(y))
+print(y.shape[0],'polymers with desired properties')
+
+# Perfom Linear and Regularized Linear Regressions on full dataset
+lin_reg = linear_model.LinearRegression().fit(y, x[:,n])
+ridge_reg = linear_model.Ridge(alpha=1.0).fit(y, x[:,n])
+lasso_reg = linear_model.Lasso(alpha=1.0).fit(y, x[:,n])
+print('Linear Regression R^2',lin_reg.score(y,x[:,n]))
+print('Ridge Regression R^2',ridge_reg.score(y,x[:,n]))
+print('Lasso Regression R^2',lasso_reg.score(y,x[:,n]),end = '\n\n')
+
+
+# Perfom k-fold cross validation to optimize alpha for both lasso and ridge regression
+alphas = np.array([0.1,0.5,1.0,2.0,5.0,100.0,200.0,100000])
+
+alpha_scores_lasso = np.zeros_like(alphas)
+k=10
+kf = KFold(n_splits=k, shuffle=True, random_state=78)
+for idx,alpha in enumerate(alphas):
+    # Initialize the lasso_reg model
+    model = linear_model.Lasso(alpha=alpha)
+
+    # Perform Cross Validation
+    scores = cross_val_score(model, x, y, cv=kf, scoring='neg_root_mean_squared_error')
+    alpha_scores_lasso[idx] = np.mean(scores) 
+print('alpha_scores_lasso:',alpha_scores_lasso)
+
+alpha_scores_ridge = np.zeros_like(alphas)
+k=10
+kf = KFold(n_splits=k, shuffle=True, random_state=78)
+for idx,alpha in enumerate(alphas):
+    # Initialize the lasso_reg model
+    model = linear_model.Ridge(alpha=alpha)
+
+    # Perform Cross Validation
+    scores = cross_val_score(model, x, y, cv=kf, scoring='neg_root_mean_squared_error')
+    alpha_scores_ridge[idx] = np.mean(scores) 
+print('alpha_scores_ridge:',alpha_scores_ridge)
+
+alpha_lasso = alphas[np.argmax(alpha_scores_lasso)]
+alpha_ridge = alphas[np.argmax(alpha_scores_ridge)]
+
+
+# Perfom test split on optimized alpha to assess model generalizability
+y_train,y_test,x_train,x_test = train_test_split(y,x,test_size = 0.1,random_state=12)
+lin_reg = linear_model.LinearRegression().fit(y_train, x_train)
+ridge_reg = linear_model.Ridge(alpha=alpha_ridge).fit(y_train, x_train)
+lasso_reg = linear_model.Lasso(alpha=alpha_lasso).fit(y_train, x_train)
+print('Test splits:')
+print('Linear Regression R^2',lin_reg.score(y_test,x_test))
+print('Ridge Regression R^2',ridge_reg.score(y_test,x_test))
+print('Lasso Regression R^2',lasso_reg.score(y_test,x_test))
+print(lin_reg.coef_)
+print(ridge_reg.coef_)
+print(lasso_reg.coef_)
